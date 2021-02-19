202ca63b717b40027289f88914cca701d3dd1fdb
zero133788 <78091170+zero133788@users.noreply.github.com>
2021-02-19 09:48:37 +0200

    Update make_config.sh

--- make_config.sh
+++ make_config.sh
@@ -12,17 +12,17 @@ cert_name_req=$cert_name".req"
 cert_name_crt=$cert_name".crt"
 cert_name_ovpn=$cert_name".ovpn"
 
-cp -f /root/openvpn_server/vars_example/vars_30/vars /root/CA/EasyRSA-v3.0.6
-cd /root/openvpn_server/EasyRSA-v3.0.6/
+cp -f /root/openvpn_server/vars_example/vars_30/vars /root/CA/EasyRSA-3.0.8
+cd /root/openvpn_server/EasyRSA-3.0.8/
 printf "\n" | ./easyrsa gen-req $cert_name nopass
-cp /root/openvpn_server/EasyRSA-v3.0.6/pki/private/$cert_name_key /root/client-configs/keys/
-cp /root/openvpn_server/EasyRSA-v3.0.6/pki/reqs/$cert_name_req /tmpCA/
-cd /root/CA/EasyRSA-v3.0.6/
+cp /root/openvpn_server/EasyRSA-3.0.8/pki/private/$cert_name_key /root/client-configs/keys/
+cp /root/openvpn_server/EasyRSA-3.0.8/pki/reqs/$cert_name_req /tmpCA/
+cd /root/CA/EasyRSA-3.0.8/
 printf "\n" | ./easyrsa import-req /tmpCA/$cert_name_req $cert_name
 printf "yes\n" | ./easyrsa sign-req client $cert_name
-cp /root/CA/EasyRSA-v3.0.6/pki/issued/$cert_name_crt /tmp
+cp /root/CA/EasyRSA-3.0.8/pki/issued/$cert_name_crt /tmp
 cp /tmp/$cert_name_crt /root/client-configs/keys/
-cp /root/openvpn_server/EasyRSA-v3.0.6/ta.key /root/client-configs/keys/
+cp /root/openvpn_server/EasyRSA-3.0.8/ta.key /root/client-configs/keys/
 cp /etc/openvpn/ca.crt /root/client-configs/keys/
 cd /root/client-configs
 ./make_config.sh $cert_name
